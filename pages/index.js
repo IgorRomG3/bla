d74852225cb2e0678cc653e12dd0f4a4d3b53787
IgorRomG3 <webroma57@gmail.com>
2018-04-26 17:35:36 +0300

    added const POST

--- pages/index.js
+++ pages/index.js
@@ -61,13 +61,13 @@ export default class Blog extends React.Component {
 
 Blog.getInitialProps = async function() {
     const PORT = process.env.PORT
-    const res = await fetch('http://localhost:3000/static/data/h1.json')
+    const res = await fetch(`http://localhost:${PORT}/static/data/h1.json`)
     const h1 = await res.json()
 
-    const res2 = await fetch('http://localhost:3000/static/data/blog-single.json')
+    const res2 = await fetch(`http://localhost:${PORT}/static/data/blog-single.json`)
     const posts = await res2.json()
 
-    const res3 = await fetch('http://localhost:3000/static/data/blog-categories.json')
+    const res3 = await fetch(`http://localhost:${PORT}/static/data/blog-categories.json`)
     const cat = await res3.json()
   
     return {
