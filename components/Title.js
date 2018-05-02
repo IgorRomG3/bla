dca92f7cea4738442e430b8814ab34cc4400eb50
IgorRomG3 <webroma57@gmail.com>
2018-05-02 12:40:24 +0300

    commented all scripts in Title component

--- components/Title.js
+++ components/Title.js
@@ -11,123 +11,124 @@ export default class Title extends React.Component {
   }
 
   componentDidMount() {
+	  console.log('bla');
 
-		var body = document.body;
-		body.classList ? body.classList.add('withJs') : body.className += ' withJs';
+		// var body = document.body;
+		// body.classList ? body.classList.add('withJs') : body.className += ' withJs';
 
-      	var back = $('.back_link');
-      	var logo = $('header .logo svg path');
-      	var date_public  = $('.date_public');
-      	var cont = $('.right_block h1, .description span');
-      	var cont = $('.description span');
-      	setTimeout(function(){
-      		back.addClass('block_show');
-      		date_public.addClass('block_show');
-      		logo.css('fill','#fff');
-			//   $('.right_block .text_desc').addClass('show');
-      	}, 300);
-      	setTimeout(function(){
-			splitText(".header_title", animateText);
-			$('.right_block .description').addClass('show');
-			$('body.withJs header .description.show::before').addClass('show');
-			splitText(".text_desc", animateText2);
-      	}, 500);
+    //   	var back = $('.back_link');
+    //   	var logo = $('header .logo svg path');
+    //   	var date_public  = $('.date_public');
+    //   	var cont = $('.right_block h1, .description span');
+    //   	var cont = $('.description span');
+    //   	setTimeout(function(){
+    //   		back.addClass('block_show');
+    //   		date_public.addClass('block_show');
+    //   		logo.css('fill','#fff');
+	// 		//   $('.right_block .text_desc').addClass('show');
+    //   	}, 300);
+    //   	setTimeout(function(){
+	// 		splitText(".header_title", animateText);
+	// 		$('.right_block .description').addClass('show');
+	// 		$('body.withJs header .description.show::before').addClass('show');
+	// 		splitText(".text_desc", animateText2);
+    //   	}, 500);
 
-      function animateText(text){
-      	var parent = $(".header_title");
-      	var span = parent.find("span");
-      	var offset;
-      	var delay = 0;
-      	TweenLite.set(span, {y: "100%", lineHeight: '5'});
-      	parent.addClass("show");
-      	span.each(function(index, el) {
-
-
-      		var currentOffset = $(el).offset().top;
-      		if(index === 0){
-      			offset = currentOffset;
-      		}
-
-      		if(currentOffset > offset + 4){
-      			delay += 0.3;
-      		}
-      		if(index === span.length - 1){
-      			TweenLite.to($(el), .1, {
-      				y: "0%",
-      				lineHeight: '1.23',
-      				delay: delay,
-      				ease: Expo.easeOut,
-
-      				onComplete: function(){
-      					parent.html(text);
-      					parent.removeAttr("style");
-      				}
-      			});
-      		}else{
-      			TweenLite.to($(el), .1, {y: "0%", lineHeight: '1.23', delay: delay, ease: Expo.easeOut});
-      		}
-
-      		offset = currentOffset;
-      	});
-
-      }
-      function animateText2(text){
-      	var parent = $(".text_desc");
-      	var span = parent.find("span");
-      	var offset;
-      	var delay = 0;
-      	TweenLite.set(span, {y: '100%', lineHeight: '8'});
-      	parent.addClass("show");
-
-      	span.each(function(index, el) {
+    //   function animateText(text){
+    //   	var parent = $(".header_title");
+    //   	var span = parent.find("span");
+    //   	var offset;
+    //   	var delay = 0;
+    //   	TweenLite.set(span, {y: "100%", lineHeight: '5'});
+    //   	parent.addClass("show");
+    //   	span.each(function(index, el) {
 
 
+    //   		var currentOffset = $(el).offset().top;
+    //   		if(index === 0){
+    //   			offset = currentOffset;
+    //   		}
 
-      		var currentOffset = $(el).offset().top;
-      		if(index === 0){
-      			offset = currentOffset;
-      		}
+    //   		if(currentOffset > offset + 4){
+    //   			delay += 0.3;
+    //   		}
+    //   		if(index === span.length - 1){
+    //   			TweenLite.to($(el), .1, {
+    //   				y: "0%",
+    //   				lineHeight: '1.23',
+    //   				delay: delay,
+    //   				ease: Expo.easeOut,
 
-      		if(currentOffset > offset + 4){
-      			delay += 0.2;
-      		}
-      		if(index === span.length - 1){
+    //   				onComplete: function(){
+    //   					parent.html(text);
+    //   					parent.removeAttr("style");
+    //   				}
+    //   			});
+    //   		}else{
+    //   			TweenLite.to($(el), .1, {y: "0%", lineHeight: '1.23', delay: delay, ease: Expo.easeOut});
+    //   		}
 
-      		TweenLite.to($(el), .1, {y: '0%',lineHeight: '2.29', delay: delay, ease: Expo.easeOut,
-      			onComplete: function(){
-      				parent.html(text);
-      				parent.removeAttr("style");
-      			}});
+    //   		offset = currentOffset;
+    //   	});
 
-      		}else{
-      			TweenLite.to($(el), .5, {y: '0%',lineHeight: '2.29', delay: delay, ease: Expo.easeOut});
-      		}
+    //   }
+    //   function animateText2(text){
+    //   	var parent = $(".text_desc");
+    //   	var span = parent.find("span");
+    //   	var offset;
+    //   	var delay = 0;
+    //   	TweenLite.set(span, {y: '100%', lineHeight: '8'});
+    //   	parent.addClass("show");
+
+    //   	span.each(function(index, el) {
 
 
 
-      		offset = currentOffset;
-      	});
+    //   		var currentOffset = $(el).offset().top;
+    //   		if(index === 0){
+    //   			offset = currentOffset;
+    //   		}
 
-      }
+    //   		if(currentOffset > offset + 4){
+    //   			delay += 0.2;
+    //   		}
+    //   		if(index === span.length - 1){
 
-      function splitText(id, callback){
-      	var el = $(id);
-      	var text = el.html().trim();
-      	//var textArrayWithBR = text.split("<br>");
-      	var textArrayWithBR = text.split();
-      	textArrayWithBR.forEach(function(element, index, array){
-      		// var textArray = element.trim().split("");
-      		array[index] = '<div><span>' + element + '</span></div>';
-      	});
-      	el.height(el.height());
-      	if($(window).width() >= 1201 ) {
-      		el.html(textArrayWithBR.join("<div>&nbsp;</div>"));
-      	}
+    //   		TweenLite.to($(el), .1, {y: '0%',lineHeight: '2.29', delay: delay, ease: Expo.easeOut,
+    //   			onComplete: function(){
+    //   				parent.html(text);
+    //   				parent.removeAttr("style");
+    //   			}});
 
-      	if(callback){
-      		callback(text);
-      	}
-      }
+    //   		}else{
+    //   			TweenLite.to($(el), .5, {y: '0%',lineHeight: '2.29', delay: delay, ease: Expo.easeOut});
+    //   		}
+
+
+
+    //   		offset = currentOffset;
+    //   	});
+
+    //   }
+
+    //   function splitText(id, callback){
+    //   	var el = $(id);
+    //   	var text = el.html().trim();
+    //   	//var textArrayWithBR = text.split("<br>");
+    //   	var textArrayWithBR = text.split();
+    //   	textArrayWithBR.forEach(function(element, index, array){
+    //   		// var textArray = element.trim().split("");
+    //   		array[index] = '<div><span>' + element + '</span></div>';
+    //   	});
+    //   	el.height(el.height());
+    //   	if($(window).width() >= 1201 ) {
+    //   		el.html(textArrayWithBR.join("<div>&nbsp;</div>"));
+    //   	}
+
+    //   	if(callback){
+    //   		callback(text);
+    //   	}
+    //   }
   }
 
   goBack() {
