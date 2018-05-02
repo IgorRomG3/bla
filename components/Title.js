2470ad30c2a7adcf6604c541378de3a344a9bffc
IgorRomG3 <webroma57@gmail.com>
2018-05-02 13:42:58 +0300

    added menu script in Title

--- components/Title.js
+++ components/Title.js
@@ -16,23 +16,48 @@ export default class Title extends React.Component {
 		var body = document.body;
 		body.classList ? body.classList.add('withJs') : body.className += ' withJs';
 
-      	var back = $('.back_link');
-      	var logo = $('header .logo svg path');
-      	var date_public  = $('.date_public');
-      	var cont = $('.right_block h1, .description span');
-      	var cont = $('.description span');
-      	setTimeout(function(){
-      		back.addClass('block_show');
-      		date_public.addClass('block_show');
-      		logo.css('fill','#fff');
-			//   $('.right_block .text_desc').addClass('show');
-      	}, 300);
-      	setTimeout(function(){
-			splitText(".header_title", animateText);
-			$('.right_block .description').addClass('show');
-			$('body.withJs header .description.show::before').addClass('show');
-			splitText(".text_desc", animateText2);
-      	}, 500);
+      	var menu = $('.top_wr');
+    		var logo = $('.logo');
+    		// var conPos = $('.content-fake-bg').position().top;
+    		var menuPos = $('.top_wr').offset().top;
+    		// var hiddenMenu = $('.nav').position().top+$('.top_wr').height();
+    		var navPos = $('.form-subscribe').offset().top - 80;
+    		var nav = $('.nav');
+    		var content = $('.content-fake-bg');
+    		var subscribe = $('.content .subscribe');
+    		// var subscribePos = $('.content .subscribe').before().position().top;
+
+    		if($(this).scrollTop()>menu.height()){
+    			menu.addClass('scroll');
+    		}else{
+    			menu.removeClass('scroll');
+    		}
+
+    		if($(this).scrollTop()> conPos){
+    			menu.addClass('changeColor');
+    			logo.addClass('black');
+    		}else{
+    			menu.removeClass('changeColor');
+    			logo.removeClass('black');
+    		}
+    		if($(this).scrollTop()> hiddenMenu){
+    			menu.addClass('opacity');
+    		}else{
+    			menu.removeClass('opacity');
+    		}
+
+
+    		if(menuPos>navPos){
+    			nav.addClass('big');
+    			subscribe.addClass('small');
+    			content.addClass('black');
+    			TweenLite.to($('.nav_wr'), 1, {transform:'matrix(1, 0, 0, 1, 0, 20)', ease: Expo.easeOut,delay: 0.1});
+    		}else{
+    			nav.removeClass('big');
+    			subscribe.removeClass('small');
+    			content.removeClass('black');
+    			TweenLite.to($('.nav_wr'), 1, {transform:'matrix(1, 0, 0, 1, 0, 0)', ease: Expo.easeOut,delay: 0.1});
+    		}
 
       function animateText(text){
       	var parent = $(".header_title");
