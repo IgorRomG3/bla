cdd0ba746f41d9063b6d9f479fba7bd4d70b010f
IgorRomG3 <webroma57@gmail.com>
2018-05-03 18:34:11 +0300

    added get request for users lists in Subscribe(test)

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 export default class Subscribe extends React.Component {
 
+  componentDidMount() {
+    fetch("https://api.unisender.com/ru/api/getLists?format=json&api_key=5c9twbzd8rqzg5bmupq9b87hff9tr6uf4pfg1aga").then((resp)=>{ return resp.json() }).then((json)=>{ console.log(json) });
+  }
+
   onFocus(e) {
     return e.target.placeholder = '';
   }
