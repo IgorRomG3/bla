a623bd79a0f43e725d27b8dc85eb71e414cfead1
IgorRomG3 <webroma57@gmail.com>
2018-05-04 15:37:22 +0300

    rename list_ids to list_id in subscribe fetch

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -20,7 +20,7 @@ export default class Subscribe extends React.Component {
     var subscribe = document.getElementsByClassName('subscribe-email')[0],
         emailText = subscribe.value.trim();
 
-    fetch(`https://api.unisender.com/ru/api/subscribe?format=json&api_key=5c9twbzd8rqzg5bmupq9b87hff9tr6uf4pfg1aga&list_id=12130797&fields[email]=${emailText}`, {method: "POST"})
+    fetch(`https://api.unisender.com/ru/api/subscribe?format=json&api_key=5c9twbzd8rqzg5bmupq9b87hff9tr6uf4pfg1aga&list_ids=12130797&fields[email]=${emailText}`, {method: "POST"})
     .then(function(res){ return res.json(); })
     .then(function(data){ console.log( JSON.stringify( data ) ) })
   }
