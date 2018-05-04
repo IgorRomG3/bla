f3aad292561f940a913a08241b53012f560a1613
IgorRomG3 <webroma57@gmail.com>
2018-05-04 12:13:41 +0300

    added fetch post to unisender

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -14,15 +14,13 @@ export default class Subscribe extends React.Component {
     return e.target.placeholder = "Подпишитесь на нашу Рассылку";
   }
 
-  getLists() {
+  postNewEmail() {
     var subscribe = document.getElementsByClassName('subscribe-email')[0],
-        emailText = subscribe.value;
+        emailText = subscribe.value.trim();
 
-    if(emailText !== '') {
-      console.log(emailText.trim());
-    }else {
-      console.log('enter email');
-    }
+    fetch(`https://api.unisender.com/ru/api/subscribe?format=json&api_key=5c9twbzd8rqzg5bmupq9b87hff9tr6uf4pfg1aga&list_ids=12130797&fields[email]=${emailText}&double_optin=0`, {method: "POST"})
+    .then(function(res){ return res.json(); })
+    .then(function(data){ console.log( JSON.stringify( data ) ) })
   }
 
   render() {
@@ -37,7 +35,7 @@ export default class Subscribe extends React.Component {
                required />
         <input type="submit"
                className="subscribe-btn"
-               onClick = {this.getLists}
+               onClick = {this.postNewEmail}
                value="" />
       </form>
     )
